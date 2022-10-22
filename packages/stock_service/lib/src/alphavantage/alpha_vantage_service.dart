898d5199c43581875d0bfd845acfc6ab9747d833
deniotokiari <deniotokiari@gmail.com>
2022-10-23 01:53:01 +0200

    add rate limiter

--- packages/stock_service/lib/src/alphavantage/alpha_vantage_service.dart
+++ packages/stock_service/lib/src/alphavantage/alpha_vantage_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:synchronized/synchronized.dart' as synchronized;
 
 import 'package:dio/dio.dart';
 import 'package:stock_service/src/alphavantage/models/new_and_sentiment_response.dart';
@@ -25,20 +26,26 @@ class AlphaVantageService implements StockServiceApi {
     responseType: ResponseType.plain,
   ));
 
-  Future<T> executeWithRequestsLimitCheck<T>(Future<T> future) => future;
+  final _rateLimit = RateLimit(
+    limit: 5,
+    rate: const Duration(minutes: 1),
+  );
+
+  Future<T> executeWithRequestsLimitCheck<T>(Future<T> Function() future) =>
+      _rateLimit.withRateLimit(future);
 
   Future<T> executeWithJsonParsing<T>(
     String function,
     Map<String, dynamic> queryParameters,
     T Function(Map<String, dynamic> json) convert,
   ) async {
-    final response = await executeWithRequestsLimitCheck(_stockApiDio.get(
-      '/query',
-      queryParameters: {
-        _function: function,
-        ...queryParameters,
-      },
-    ));
+    final response = await executeWithRequestsLimitCheck(() => _stockApiDio.get(
+          '/query',
+          queryParameters: {
+            _function: function,
+            ...queryParameters,
+          },
+        ));
 
     return convert(response.data);
   }
@@ -129,4 +136,44 @@ extension _DateTimeExt on DateTime {
 
     return '$y$m${d}T$h$m';
   }
+}
+
+class RateLimit {
+  final int limit;
+  final Duration rate;
+
+  final _lock = synchronized.Lock();
+
+  var _requestsPerformed = 0;
+  var _firstRequestInSequence = DateTime.now().millisecondsSinceEpoch;
+
+  RateLimit({
+    required this.limit,
+    required this.rate,
+  });
+
+  Future<T> withRateLimit<T>(Future<T> Function() future) => _lock.synchronized(() async {
+        final timeSinceFirstRequest = Duration(
+          milliseconds: DateTime.now().millisecondsSinceEpoch - _firstRequestInSequence,
+        );
+
+        if (_requestsPerformed == limit) {
+          if (timeSinceFirstRequest < rate) {
+            final waitTime = rate - timeSinceFirstRequest;
+            print('LOG_: ${DateTime.now()}: wait => $waitTime');
+            await Future.delayed(waitTime);
+          }
+
+          _requestsPerformed = 0;
+          _firstRequestInSequence = DateTime.now().millisecondsSinceEpoch;
+        } else if (timeSinceFirstRequest > rate) {
+          _requestsPerformed = 0;
+          _firstRequestInSequence = DateTime.now().millisecondsSinceEpoch;
+        }
+
+        _requestsPerformed++;
+        print('LOG_: ${DateTime.now()}: request => $_requestsPerformed');
+
+        return future();
+      });
 }
