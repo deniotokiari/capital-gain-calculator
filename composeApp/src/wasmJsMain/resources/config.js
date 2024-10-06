370ce20994f843728384ee06225b46cd72a8a3e8
deniotokiari <deniotokiari@gmail.com>
2024-10-06 17:26:01 +0200

    add web support for firebase store

--- composeApp/src/wasmJsMain/resources/config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAduR3UuI5IhBw-xf2aQcT6QBComW6n80Y",
-    authDomain: "capital-gain-calculator-b48d8.firebaseapp.com",
-    projectId: "capital-gain-calculator-b48d8",
-    storageBucket: "capital-gain-calculator-b48d8.appspot.com",
-    messagingSenderId: "919173750264",
-    appId: "1:919173750264:web:ab834f61c64ccd93bc0a0d",
-    measurementId: "G-V4KGEKC7H2"
-};
