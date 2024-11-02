a96cf3e2a06b94642df403482c5ecbcc3f409f2d
deniotokiari <deniotokiari@gmail.com>
2024-11-02 11:42:33 +0100

    testing tests summary

---
+++ gateway/src/androidUnitTest/kotlin/pl/deniotokiari/capital/gain/calculator/gateway/domain/usecase/GetUsdCurrencyUseCaseTest.kt
@@ -0,0 +1,12 @@
+package pl.deniotokiari.capital.gain.calculator.gateway.domain.usecase
+
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class GetUsdCurrencyUseCaseTest {
+    @Test
+    fun `Test fail`() = runTest {
+        assertTrue(false)
+    }
+}
