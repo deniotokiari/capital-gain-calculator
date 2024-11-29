f609983545b4a0659c2567c1abf51541f690e433
deniotokiari <deniotokiari@gmail.com>
2024-11-29 16:49:05 +0100

    bump versions

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -8,6 +8,10 @@ kotlin {
             implementation(project(":core:misc"))
         }
 
+        androidUnitTest.dependencies {
+            implementation(libs.bundles.testing)
+        }
+
         androidMain.dependencies {
             implementation(libs.kotlinx.coroutines.test)
         }
